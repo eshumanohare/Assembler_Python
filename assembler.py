a27c102dbfa2244c2c104c066bca270834643bc5
eshumanohare <37545945+eshumanohare@users.noreply.github.com>
2020-03-11 01:13:31 +0530

    Add files via upload

--- assembler.py
+++ assembler.py
@@ -7,7 +7,7 @@ LC = 1
 
 random_var_val = 1
 
-opcode_dict = dict (CLA= "0000", LAC= "0001", SAC= "0010", ADD= "0011", SUB= "0100", BRZ= "0101", BRN= "0110", BRP= "0111", INP= "1000", DSP=0b1001,
+opcode_dict = dict (CLA= "0000", LAC= "0001", SAC= "0010", ADD= "0011", SUB= "0100", BRZ= "0101", BRN= "0110", BRP= "0111", INP= "1000", DSP= "1001",
                     MUL= "1010", DIV= "1011", STP= "1100")
 
 # dictionary class defined for ease addition of items
@@ -65,6 +65,57 @@ def firstpass():
 	print(variable_dict)
 	print(label_dict)
 
+def secondPass():
+	
+	global LC
+	LC = 0
+	
+	input_list = input_file.split("\n")
+	
+	for items in input_list:
+		
+		if items == "CLA":
+			object_code_file.writelines(opcode_dict.get(items) * 3 + "\n")
+		
+		if items[0:3] == "INP":
+			val = items.split(" ")
+			object_code_file.writelines(opcode_dict.get(val[0]) + variable_dict.get((val[1])) + "\n")
+			
+		if items[0:3] == "LAC":
+			val = items.split(" ")
+			object_code_file.writelines(opcode_dict.get(val[0]) + variable_dict.get(val[1]) + "\n")
+		
+		if items[0:3] == "SUB":
+			val = items.split(" ")
+			object_code_file.writelines(opcode_dict.get(val[0]) + variable_dict.get(val[1]) + "\n")
+			
+		if items[0:3] == "BRN":
+			val = items.split (" ")
+			object_code_file.writelines (opcode_dict.get (val [0]) + label_dict.get (val [1]) + "\n")
+		
+		if items [0:3] == "BRP":
+			val = items.split (" ")
+			object_code_file.writelines (opcode_dict.get (val [0]) + label_dict.get (val [1]) + "\n")
+		
+		if items [0:3] == "BRZ":
+			val = items.split (" ")
+			object_code_file.writelines (opcode_dict.get (val [0]) + label_dict.get (val [1]) + "\n")
+		
+		if items [0:3] == "DSP":
+			val = items.split (" ")
+			object_code_file.writelines (opcode_dict.get (val [0]) + variable_dict.get (val [1]) + "\n")
+		
+		if items[0:2] in label_dict:
+			val = items.split(" ")
+			if val[1] == "STP":
+				object_code_file.writelines (opcode_dict.get(val [1]) + "0"*8+ "\n")
+				continue
+			else:
+				object_code_file.writelines(opcode_dict.get(val[1]) + variable_dict.get(val[2]) + "\n")
+				continue
+		
+	
+	
 
 			
 	#print(input_list)
@@ -72,3 +123,4 @@ def firstpass():
 		#print(line, end = "")
 
 firstpass()
+secondPass()
